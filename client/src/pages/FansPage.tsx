9d4441c23e81226447bf1612a847e9d7b4f49236
Carlos Chavarria Quiros <carlos.chavarriaquiros@ucr.ac.cr>
2026-09-17 20:37:48 -0600

    fixed typo

--- client/src/pages/FansPage.tsx
+++ client/src/pages/FansPage.tsx
@@ -92,7 +92,7 @@ export default function FansPage() {
         <p className="muted">No fans match your search.</p>
       )}
 
-      <div className="gr`id`">
+      <div className="grid">
         {visibleFans.map((fan) => (
           <FanCard key={fan.id} fan={fan} />
         ))}
